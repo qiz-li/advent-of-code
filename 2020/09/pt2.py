97f1fc73cc2fdf3580481d979674d392d23958c3
qiz-li <47799695+qiz-li@users.noreply.github.com>
2021-01-06 14:28:43 -0800

    ⚡️  refactor: Optimize `Day 9`

--- 2020/09/pt2.py
+++ 2020/09/pt2.py
@@ -6,8 +6,9 @@ with open('input.txt', 'r') as file:
 for idx in range(len(puzzle_input)):
     numbers_sum = 0
     numbers_lst = []
-    # Try each number in the list
-    for num_try in puzzle_input:
+    # Try each number in the list.
+    # No need to keep on adding if the sum is already above our number.
+    while numbers_sum <= 167829540:
         # The value of the first number and
         # each number after the first is added to the "numbers_sum"
         numbers_sum += puzzle_input[idx]
@@ -16,12 +17,8 @@ for idx in range(len(puzzle_input)):
         # If "numbers_sum" adds up to our number,
         # we have found our list of numbers!
         if numbers_sum == 167829540 and len(numbers_lst) != 1:
-            numbers_lst.sort()
-            print(numbers_lst[0] + numbers_lst[-1])
+            print(min(numbers_lst) + max(numbers_lst))
             break
-        # No need to keep on adding if the sum is already above our number
-        elif numbers_sum > 167829540:
-            break
-    # We already have our list of numbers, no need to keep on looping.
-    if numbers_sum == 167829540 and len(numbers_lst) != 1:
-        break
+    else:
+        continue
+    break
