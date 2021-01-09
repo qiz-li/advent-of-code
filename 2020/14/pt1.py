a9ddb2b7e17c79ca6f3020f572cb903143a8df53
qiz-li <47799695+qiz-li@users.noreply.github.com>
2021-01-08 19:25:44 -0800

    ✨  feat: Add `Day 14 #1`

---
+++ 2020/14/pt1.py
@@ -0,0 +1,37 @@
+# Find this puzzle at:
+# https://adventofcode.com/2020/day/14
+
+with open('input.txt', 'r') as file:
+    puzzle_input = file.read().splitlines()
+memory = {}
+
+
+def apply_mask(xy):
+    """
+    Apply bitmask to value.
+
+    Args:
+    xy: A tuple with the bit of the mask
+    and the bit to mask (mask, value).
+
+    Returns:
+    The bit after being masked.
+    """
+    if value[0] == 'X':
+        return value[1]
+    else:
+        return value[0]
+
+
+for line in puzzle_input:
+    command, value = line.split(' = ')
+    # Store the new mask
+    if command == 'mask':
+        mask = value
+    # Mask the value and store its decimal value in memory
+    else:
+        address = int(''.join(i for i in command if i.isdigit()))
+        value = bin(int(value))[2:].zfill(36)
+        write_value = ''.join(map(apply_mask, zip(mask, value)))
+        memory[address] = int(write_value, 2)
+print(sum(memory.values()))
